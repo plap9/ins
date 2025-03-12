36ea5608d5b4de5e07dbaecf4072a1634c34d9fc
lap <plap4340@gmail.com>
2025-03-12 23:11:53 +0700

    add api register and login

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,19 +1,22 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import testRoutes from "./routes/test"; 
+import authRouter from "./routes/auth";
 
 dotenv.config();
-
-const app = express(); 
-
+const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
 
-app.use("/api", testRoutes);
+app.use("/auth", authRouter);
+
+app.use((err: any, req: express.Request, res: express.Response, next: express.NextFunction) => {
+    console.error("Global error:", err.stack);
+    res.status(500).json({ message: "Something went wrong", error: err.message });
+  });
 
 app.listen(PORT, () => {
-  console.log(`✅ Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
