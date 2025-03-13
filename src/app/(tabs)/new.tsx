009f8e96088cb2e9a371fd6040af6af9afb1911f
doanthang <doanthang304204@gmail.com>
2025-03-14 02:11:49 +0700

    Add frontend progress

---
+++ src/app/(tabs)/new.tsx
@@ -0,0 +1,6 @@
+
+import { Text } from "react-native";
+
+export default function About() {
+    return <Text>Crete Post !</Text>;
+}
---
+++ src/app/index.tsx
@@ -0,0 +1,5 @@
+import { Text } from "react-native";
+import { Link, Redirect } from "expo-router";
+export default function Home() {
+    return <Redirect href="/(tabs)"/>;
+} 
