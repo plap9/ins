277359d709ca42bdc846db43788603fc2f692585
doanthang <doanthang304204@gmail.com>
2025-03-15 21:00:04 +0700

    create post screen update function image picker

--- src/app/(tabs)/new.tsx
+++ src/app/(tabs)/new.tsx
@@ -1,26 +1,53 @@
 
 import { Text, View, Image, TextInput, Pressable } from "react-native";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import * as ImagePicker from 'expo-image-picker';
 
 export default function CreatePost() {
     const [caption, setCaption] = useState('');
+    const [image, setImage] = useState<string | null>(null);
+
+    useEffect(() => {
+        if(!image) {
+            pickImage();
+        }
+    }, [image]);
+
+    const pickImage = async () => {
+        // No permissions request is necessary for launching the image library
+        let result = await ImagePicker.launchImageLibraryAsync({
+          mediaTypes: ['images', 'videos'],
+          allowsEditing: true,
+          aspect: [3, 4],
+          quality: 1,
+        });
+    
+        console.log(result);
+    
+        if (!result.canceled) {
+          setImage(result.assets[0].uri);
+        }
+      };
+
     return (
         <View className="p-3 items-center flex-1">
             {/* Image Picker */}
+            {image ? (
             <Image
-                source={{
-                    uri: 'https://anhnail.vn/wp-content/uploads/2024/10/anh-meme-ech-xanh-5.webp'
-                }}
-                className='w-2/3 aspect-[3/4] rounded-lg shadow-md'
-            />
-            <Text onPress={() => {}} className="text-blue-500 font-semibold m-3 ">
+                source={{ uri: image }}
+                className='w-2/3 aspect-[3/4] rounded-lg bg-slate-300'/>
+            ) : (
+                <View className="w-2/3 aspect-[3/4] rounded-lg bg-slate-300"/>
+            )}
+
+            <Text onPress={pickImage} className="text-blue-500 font-semibold m-3 ">
                 Change
             </Text>
             
             {/* Text Input for caption */}
             <TextInput 
                 value={caption}
-                onChange={(newValue) => setCaption(newValue)}
+                onChangeText={(text) => setCaption(text)}
                 placeholder="Write a caption..."
                 className="bg-slate-100 w-full p-3 rounded-lg"
             />
