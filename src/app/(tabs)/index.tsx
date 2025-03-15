06ca25b4846e883a018b96afdd17106f9b86964d
doanthang <doanthang304204@gmail.com>
2025-03-15 18:44:30 +0700

    responsive feedscreen

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -6,8 +6,9 @@ import posts from "../../../assets/data/posts.json";
 export default function FeedScreen() {
     return (
     <FlatList
+        className="items-center"
         data={posts}
-        contentContainerStyle={{gap:10}}
+        contentContainerStyle={{gap:10, maxWidth: 512, flex: 1, width: '100%',}}
         renderItem={({item}) => <PostListItem posts={item} />}
         showsVerticalScrollIndicator={false}
     />
