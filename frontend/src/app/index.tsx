b9886d3902eaeb95597a32120fa32ed9c40d09cc
lap <plap4340@gmail.com>
2025-03-23 16:19:38 +0700

    login screen

--- frontend/src/app/index.tsx
+++ frontend/src/app/index.tsx
@@ -1,11 +1,16 @@
 import { Redirect } from "expo-router";
 import { useAuth } from "./context/AuthContext";
+import { View, ActivityIndicator } from "react-native";
 
 export default function Index() {
   const { authData, isLoading } = useAuth();
   
   if (isLoading) {
-    return null; 
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <ActivityIndicator size="large" color="#0000ff" />
+      </View>
+    ); 
   }
   
   return authData ? <Redirect href="/(tabs)" /> : <Redirect href="/auth/login" />;
--- src/app/index.tsx
+++
@@ -1,6 +0,0 @@
-import { Text } from "react-native";
-import { Link, Redirect } from "expo-router";
-
-export default function Home() {
-    return <Redirect href="/(tabs)"/>;
-} 
