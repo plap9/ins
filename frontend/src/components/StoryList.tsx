9ed28185432bb01f3e0bb1a9ca689aaa543040a3
doanthang <doanthang304204@gmail.com>
2025-03-27 22:50:53 +0700

    rewrite component story

--- frontend/src/components/StoryList.tsx
+++ frontend/src/components/StoryList.tsx
@@ -4,26 +4,35 @@ import { View, Text, Image, ScrollView } from "react-native";
 interface Story {
   id: string;
   image: string;
-  title: string;
+  title?: string;
+  username?: string;
+  hasStory?: boolean;
+  isYourStory?: boolean;
+  isOpened?: boolean;
+  isHighlight?: boolean;
 }
 
 interface HighlightsStoryProps {
   stories: Story[];
 }
 
-const HighlightsStory = ({ stories }: HighlightsStoryProps): JSX.Element => {
+const StoryList = ({ stories }: HighlightsStoryProps): JSX.Element => {
   return (
     <View className="mb-4">
       <ScrollView horizontal showsHorizontalScrollIndicator={false}>
         {stories.map((story) => (
           <View key={story.id} className="items-center mr-4">
-            <View className="w-16 h-16 rounded-full border border-gray-300 p-0.5">
+            <View className={`
+             ${story.hasStory ? (story.isOpened ? "border-pink-500 border-2" : "border-gray-300 border-2") : ""} 
+             ${story.isHighlight ? "w-16" : "w-24"}
+             aspect-square rounded-full border border-gray-300 p-0.5 
+            `}>
               <Image
                 source={{ uri: story.image }}
                 className="w-full h-full rounded-full"
               />
             </View>
-            <Text className="mt-1 text-xs text-center">{story.title}</Text>
+            <Text className="mt-1 text-xs text-center">{story.title || story.username}</Text>
           </View>
         ))}
       </ScrollView>
@@ -31,4 +40,4 @@ const HighlightsStory = ({ stories }: HighlightsStoryProps): JSX.Element => {
   );
 };
 
-export default HighlightsStory;
+export default StoryList;
