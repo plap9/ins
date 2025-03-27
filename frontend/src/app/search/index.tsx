f33c9d522806a5a9f040226eccc708fd0dc8a0b2
doanthang <doanthang304204@gmail.com>
2025-03-28 00:14:10 +0700

    add searchscreen when press search bar

--- frontend/src/app/search/index.tsx
+++ frontend/src/app/search/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { View, Text, TextInput, FlatList, Image, TouchableOpacity } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { Search, Heart, MoreVertical } from "lucide-react-native";
+import { useRouter } from "expo-router";
 
 const exploreImages = [
   "https://cdn-useast1.kapwing.com/static/templates/spider-man-triple-meme-template-full-a9a8b78a.webp",
@@ -20,19 +21,22 @@ const exploreImages = [
 ];
 
 const SearchScreen = () => {
+  const router = useRouter();
   return (
     <View>
       {/* Thanh tìm kiếm */}
-      <View className="flex-row items-center px-4 py-2 border-b border-gray-200 flex-11 ">
+      <View className="flex-row items-center px-4 py-2 border-b border-gray-200 flex-11">
         <View className="flex-1 bg-gray-100 flex-row items-center px-3 py-2 rounded-full">
-          <Search size={20} className="text-gray-500 mr-2" />
-          <TextInput
-            placeholder="Tìm kiếm"
-            placeholderTextColor="#aaa"
-            className="flex-1 text-black"
-          />
-        </View>
+          {/* <Search size={20} className="text-gray-500 mr-2" /> */}
+          <TouchableOpacity
+          className="bg-gray-100 rounded-md p-2 flex-row items-center"
+          onPress={() => router.push("/search/searchbarscreen")}
+        >
+          <Search size={20} className="text-gray-500 mr-2" /> 
+          <Text className="ml-2 text-gray-500">Search</Text>
+        </TouchableOpacity>
       </View>
+        </View>
 
       {/* Lưới ảnh */}
       <View style={{flexDirection:"row", flexWrap:"wrap", justifyContent:"space-between"}}>
