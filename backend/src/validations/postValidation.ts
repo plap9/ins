20511e8368caa114f07552615b480b35b758fcb1
lap <plap4340@gmail.com>
2025-03-31 09:20:03 +0700

    Revert "connect api comment full opt"
    This reverts commit eb3487d6e8ffe473647134648808c6ce9c9ed906.

--- backend/src/validations/postValidation.ts
+++
@@ -1,9 +0,0 @@
-import Joi, { ObjectSchema } from "joi";
-
-export const postSchema: ObjectSchema = Joi.object({
-    content: Joi.string().allow('').optional(),
-    location: Joi.string().allow('').optional(),
-    privacy: Joi.string().valid('public', 'friends', 'private').default('public').messages({
-        "any.only": "Quyền riêng tư phải là 'public', 'friends' hoặc 'private'",
-    }),
-});
